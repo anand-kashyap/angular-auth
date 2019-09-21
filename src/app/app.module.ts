7996c52ebe34cde0f52ef1ed564c5ac4054d745a
anand-kashyap <anandkashyap60@gmail.com>
2019-09-21 17:45:41 +0530

    fix port and formgroup error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonsModule } from 'ngx-bootstrap';
@@ -9,17 +10,18 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonsModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
   declarations: [
     AppComponent,
     LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
